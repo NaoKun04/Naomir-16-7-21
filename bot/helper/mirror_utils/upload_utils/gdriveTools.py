e3073517bac47b3cd62d0e7643c93843c819980c
NaoKun04 <87535232+NaoKun04@users.noreply.github.com>
2021-07-16 23:44:46 +0700

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -115,7 +115,7 @@ class GoogleDriveHelper:
                                      resumable=False)
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded using Slam Mirror Bot',
+            'description': 'Uploaded using Naō Mirror Bot',
             'mimeType': mime_type,
         }
         if parent_id is not None:
@@ -169,7 +169,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Slam Mirror Bot',
+            'description': 'Uploaded by Naō Mirror Bot',
             'mimeType': mime_type,
         }
         try:
@@ -535,9 +535,9 @@ class GoogleDriveHelper:
                     content += f'<b> | <a href="https://telegra.ph/{self.path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             Telegraph(access_token=telegraph_token).edit_page(path = self.path[prev_page],
-                                 title = 'Slam Mirror Bot Search',
-                                 author_name='Slam Mirror Bot',
-                                 author_url='https://github.com/breakdowns/slam-mirrorbot',
+                                 title = 'Naō Mirror Bot Search',
+                                 author_name='Naō Mirror Bot',
+                                 author_url='https://t.me/@KosakaNao_46',
                                  html_content=content)
         return
 
@@ -620,9 +620,9 @@ class GoogleDriveHelper:
 
             for content in self.telegraph_content :
                 self.path.append(Telegraph(access_token=telegraph_token).create_page(
-                                                        title = 'Slam Mirror Bot Search',
-                                                        author_name='Slam Mirror Bot',
-                                                        author_url='https://github.com/breakdowns/slam-mirrorbot',
+                                                        title = 'Naō Mirror Bot Search',
+                                                        author_name='Naō Mirror Bot',
+                                                        author_url='https://t.me/@KosakaNao_46',
                                                         html_content=content
                                                         )['path'])
 
